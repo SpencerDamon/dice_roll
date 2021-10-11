fecdd263e629a2427c94939eea04a6a60679efdf
SpencerDamon <90030235+SpencerDamon@users.noreply.github.com>
2021-10-10 22:37:11 -0700

    Update dice_roll.kt
    passed a integer value into the Dice class to allow for multiple sided dice to be instanciated easily
    changed hard coded intRange to a variable

--- dice_roll.kt
+++ dice_roll.kt
@@ -1,14 +1,14 @@
 // This app rolls a dice, and returns the roll value
 //updates soon, to a variable sided dice, and multiple dice roll
 fun main(){
-  val dice = Dice()
-  println("${dice.roll()}")
+  val myFirstDice = Dice(6)
+  println("${myFirstDice.roll()}")
 
 }
 //This rolls the dice and returns a random number between 1 and 6
-class Dice(){
+class Dice(val numSides: Int){
   fun roll(): Int{
-    val randomNumber = (1..6).random()
+    val randomNumber = (1..numsides).random()
     return randomNumber
   
   }
